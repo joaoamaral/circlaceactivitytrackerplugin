d221db0a2f5b7270ff4772c119ec229795e76190
João Amaral <jamaral@pasonatquila.com>
2020-04-09 16:19:26 +0900

    added axios and fixed axios.post no auth header issue

--- index.js
+++ index.js
@@ -2,23 +2,27 @@
 import axios from 'axios'
 
 export default {
-    install (Vue, { router, endpoint, jwt, uuid}) {
-      router.beforeEach((to, from, next) => {
-        axios.post(endpoint, {
-          headers: {
-            crossdomain: true,
-            'Authorization': `${jwt}`,
-            'content-type': 'application/json'
-          },
-          activity: "pageview",
-          useruuid: `${uuid}`,
-          context: {
-              url: `${window.location.hostname}${to.fullPath}`,
-              useragent: `${navigator.userAgent}`,
-              resolution: `${screen.availWidth} x ${screen.availHeight} (${window.innerWidth} x ${window.innerHeight})`,
+  install (Vue, { router, endpoint, jwt, uuid}) {
+    router.beforeEach((to, from, next) => {
+      axios({
+        "method": "POST",
+        "url": endpoint,
+        "headers": {
+          "content-type": "application/json",
+          "Authorization": `${jwt}`
+        },
+        "data": {
+          "activity": "pageview",
+          "operation": "PAGEVIEW related operation 2020-04-09T05:36:58+00:00",
+          "useruuid": `${uuid}`,
+          "context": {
+            url: `${window.location.hostname}${to.fullPath}`,
+            useragent: `${navigator.userAgent}`,
+            resolution: `${screen.availWidth} x ${screen.availHeight} (${window.innerWidth} x ${window.innerHeight})`,
           }
-        })
-        next()
+        }
       })
-    }
+      next()
+    })
   }
+}
